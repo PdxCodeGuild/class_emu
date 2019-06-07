02f0894dd9cccd2a62fefbb6c518274585d6602e
Kevin <wrenJTD@gmail.com>
2019-06-07 15:41:36 -0700

    lab4version2

---
+++ Code/Kevin/lab4version2.py
@@ -0,0 +1,8 @@
+print("Welcome, USERNAME, to the Magic 8-Ball program!\n")
+user_Input = input("Go ahead, ask the Magic 8-Ball a question. PS. Make it challenging!\n")
+magic_Answers = ['Probably', 'Most Definitely', 'I don\'t see it in the cards']
+import random
+print(random.choice(magic_Answers))
+while user_Input != 'DONE' and user_Input != 'done':
+    user_Input = input("Have another question? Enter done to quit\n")
+    print(random.choice(magic_Answers))
