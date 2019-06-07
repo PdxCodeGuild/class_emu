cc7087211b9b06814fa2eaa7e1824774c17cd198
Kevin <wrenJTD@gmail.com>
2019-06-07 16:13:40 -0700

    lab5 recommit

--- Code/Kevin/lab5.py
+++ Code/Kevin/lab5.py
@@ -4,4 +4,4 @@ my_Noses = ['-', '^']
 my_Mouths = [')', '(', '|', ' L', 'P']
 
 print("Welcome to the face generator! Here's a free face! \n")
-print(random.choice(my_Eyes), random.choice(my_Noses), random.choice(my_Mouths))
+print(random.choice(my_Eyes)+random.choice(my_Noses)+random.choice(my_Mouths))
