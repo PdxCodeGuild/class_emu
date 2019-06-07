3579562ff7c9b0fd0593489026124aca8d436432
larry <lmoiola@gmail.com>
2019-06-06 22:23:46 -0700

    adding random.shuffle to list of adjectives

--- Code/Larry/lab02_v2b_mad_libs.py
+++ Code/Larry/lab02_v2b_mad_libs.py
@@ -9,7 +9,7 @@ Version 2 (optional)
 Make a functional solution that utilizes lists. For example, ask the user for 3 adjectives, separated by commas, then use the .split() function to store each adjective and later use it in your story.
 Add randomness! Use the random module, rather than selecting which adjective goes where in the story.
 '''
-
+import random
 # Ask the user for three adjectives that you'll put in the Mad Lib
 list_adj = [] # base case
 for num in range(0,3):
@@ -17,8 +17,14 @@ for num in range(0,3):
     # create a list of adjectives
     list_adj.append(user_adj)
 
+# Add randomness! Use the random module, rather than selecting which adjective goes where in the story.
+random.shuffle(list_adj)
+
 # Use string concatenation to put each word into the Mad Lib using the items in the list
 print(f"VACATIONS: A vacation is when you take a trip to some {list_adj[0]} place with your {list_adj[1]} family. Usually you go to some place that is near a/an {list_adj[2]} beach or up on a mountain. A good vacation place is one where you can ride donkeys or play Scrabble.")
+
+# # Use string concatenation to put each word into the Mad Lib using the items in the list
+# print(f"VACATIONS: A vacation is when you take a trip to some {list_adj[0]} place with your {list_adj[1]} family. Usually you go to some place that is near a/an {list_adj[2]} beach or up on a mountain. A good vacation place is one where you can ride donkeys or play Scrabble.")
 
 '''
 Example:
