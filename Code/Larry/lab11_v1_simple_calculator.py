b9744e0a2daf3b520a0ac063e590db7dc63fcafa
larry <lmoiola@gmail.com>
2019-06-09 14:27:29 -0700

    version 1 of lab 11: removed sample i/o from description

--- Code/Larry/lab11_v1_simple_calculator.py
+++ Code/Larry/lab11_v1_simple_calculator.py
@@ -4,12 +4,6 @@ Lab 11: Simple Calculator
 Let's write a simple REPL (read evaluate print loop) calculator that can handle addition, subtraction, multiplication,
 and division. Ask the user for an operator and each operand. Don't forget that input returns a string,
 which you can convert to a float using float(user_input) where user_input is the string you got from input.
-
-Below is some sample input/output.
-> What is the operation you'd like to perform? +
-> What is the first number? 5
-> What is the second number? 12
-> 5 + 12 = 17
 '''
 # Import operator function (to convert string to operator)
 import operator
