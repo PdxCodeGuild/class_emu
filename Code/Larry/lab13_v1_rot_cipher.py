4b7f183aaa640d290b515aeaf6929a5e0abcb653
larry <lmoiola@gmail.com>
2019-06-13 15:24:52 -0700

    pull alphabet from strings module, generate rot13

--- Code/Larry/lab13_v1_rot_cipher.py
+++ Code/Larry/lab13_v1_rot_cipher.py
@@ -11,10 +11,14 @@ Index	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25
 English	a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z
 ROT+13	n	o	p	q	r	s	t	u	v	w	x	y	z	a	b	c	d	e	f	g	h	i	j	k	l	m
 '''
-
-# define base_alphabet and rot13_alphabet strings
-base_alphabet = 'abcdefghijklmnopqrstuvwxyz'
-rot13_alphabet = 'nopqrstuvwxyzabcdefghijklm'
+import string
+# Create base_alphabet from string library
+base_alphabet = string.ascii_lowercase
+# Generate rot13_alphabet based on base_alphabet # rot13_alphabet = 'nopqrstuvwxyzabcdefghijklm'
+rot13_alphabet_list = []
+for letter in base_alphabet:
+    rot13_alphabet_list.append(chr((ord(letter) - ord('a') + 13) % 26 + ord('a')))
+    rot13_alphabet = ''.join(rot13_alphabet_list)
 
 # prompt the user for a string
 user_string = input("Enter a string to ROT13 encrypt: ")
