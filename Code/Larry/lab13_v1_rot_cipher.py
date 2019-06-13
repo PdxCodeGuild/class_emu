ca7226710b51ffec0a00eafddc155f46d49f0d0e
larry <lmoiola@gmail.com>
2019-06-13 14:43:25 -0700

    version 1 of lab13: rot cipher

---
+++ Code/Larry/lab13_v1_rot_cipher.py
@@ -0,0 +1,32 @@
+# filename: lab13_v1_rot_cipher.py
+'''
+Lab 13: ROT Cipher
+
+Write a program that prompts the user for a string, and encodes it with ROT13.
+For each character, find the corresponding character, add it to an output string.
+Notice that there are 26 letters in the English language,
+so encryption is the same as decryption.
+
+Index	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25
+English	a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z
+ROT+13	n	o	p	q	r	s	t	u	v	w	x	y	z	a	b	c	d	e	f	g	h	i	j	k	l	m
+'''
+
+# define base_alphabet and rot13_alphabet strings
+base_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+rot13_alphabet = 'nopqrstuvwxyzabcdefghijklm'
+
+# prompt the user for a string
+user_string = input("Enter a string to ROT13 encrypt: ")
+
+# for each character, find the corresponding character
+rot13_letters = []
+for letter in user_string:
+    id_for_letter = base_alphabet.find(letter)
+    # add corresponding character to an output string
+    rot13_letters.append(rot13_alphabet[id_for_letter])
+    # convert list to string
+    rot13_string = ''.join(rot13_letters)
+
+# print
+print(rot13_string)
