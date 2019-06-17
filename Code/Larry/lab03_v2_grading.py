60123eea117056c7dc24da42529b4efd9b43b9e0
larry <lmoiola@gmail.com>
2019-06-16 21:02:46 -0700

    updates comments

--- Code/Larry/lab03_v2_grading.py
+++ Code/Larry/lab03_v2_grading.py
@@ -1,8 +1,12 @@
 # filename: lab03_v2b_grading.py
 '''
+Lab 3: Grading
+Let's convert a number grade to a letter grade, using if and elif statements and comparisons.
+
 Instructions:
 Have the user enter a number representing the grade (0-100)
 Convert the number grade to a letter grade
+
 Numeric Ranges:
 A+ 97–100
 A	93–96
@@ -17,6 +21,7 @@ D+	67–69
 D	63–66
 D-	60–62
 F	0–59
+
 Version 2
 Find the specific letter grade (A+, B-, etc).
 You can check for more specific ranges using if statements, or use modulus % to get the ones-digit to
@@ -28,19 +33,16 @@ Then you can concatenate that string with your grade string.
 while True:
     # get user input (0 - 100)
     numeric_grade = input("Enter a number between 0 and 100: ")
-    if numeric_grade.isdigit():
-        numeric_grade = int(numeric_grade)
-        # check that input is between 0 and 100
-        if 0 <= numeric_grade <= 100:
-            # convert the number grade to a letter grade
-            # then, print the result: letter grade
-            # use modulus % to get the ones-digit
-            plus_minus = ''
-            if numeric_grade % 10 >= 7:
-                plus_minus = '+' # set plus_minus equal to plus (+)
-            elif numeric_grade % 10 <= 2:
-                plus_minus = '-' # set plus_minus equal to minus (-)
-            if 90 <= numeric_grade <= 100:
+    if numeric_grade.isdigit(): # validate input: if input is < 0 or > 100, print error message & let user try again
+        numeric_grade = int(numeric_grade)  # convert input to integer
+        if 0 <= numeric_grade <= 100:       # check that input is between 0 and 100
+            # convert the number grade to a letter grade, then print the result: letter grade
+            plus_minus = '' # base value for string
+            if numeric_grade % 10 >= 7:         # use modulus % to get the ones-digit
+                plus_minus = '+'                # set plus_minus equal to plus (+)
+            elif numeric_grade % 10 <= 2:       # use modulus % to get the ones-digit
+                plus_minus = '-'                # set plus_minus equal to minus (-)
+            if 90 <= numeric_grade <= 100:      # if numeric_grade is in the range, print the letter grade
                     print(f"You entered {numeric_grade}. That's an A{plus_minus}. Great job!")
             elif 80 <= numeric_grade <= 90:
                     print(f"You entered {numeric_grade}. That's a B{plus_minus}. Good job!")
@@ -48,8 +50,7 @@ while True:
                     print(f"You entered {numeric_grade}. That's a C{plus_minus}. Not bad!")
             elif 60 <= numeric_grade <= 70:
                     print(f"You entered {numeric_grade}. That's a D{plus_minus}. Umm, did you study?!")
-            else:
+            else:                               # if numeric_grade is <= 59
                 print(f"You entered {numeric_grade}. That's an F. D'oh!")
             break
-    # if input is < 0 or > 100, print error message and let' user try again
     print(f"You entered {numeric_grade}. That's not a number between 0 and 100. ")
