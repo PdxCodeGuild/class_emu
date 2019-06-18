9bc5dc4578a71e09f34139a302bddba9d08635cd
larry <lmoiola@gmail.com>
2019-06-17 19:10:07 -0700

    clean up comments

--- Code/Larry/lab09_v2_unit_converter.py
+++ Code/Larry/lab09_v2_unit_converter.py
@@ -30,7 +30,6 @@ num_dict = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
 first_num = input("What is the first number? ")
 second_num = input("What is the second number? ")
 print(f"The sum is {num_dict[first_num] + num_dict[second_num]}")
-
 '''
 
 # Ask the user for the number of feet
--- Code/Larry/lab09_v4_unit_converter.py
+++ Code/Larry/lab09_v4_unit_converter.py
@@ -1,14 +1,14 @@
 # filename: unit_converter_v4.py
-
 '''
 Lab 9: Unit Converter
 
-Note:
-See unit_converter.py, unit_converter_v2.py, and unit_converter_v3.py for previous objectives.
+This lab will involve writing a program that allows the user to convert a number between units.
 
 Version 4
 Now we'll ask the user for the distance, the starting units, and the units to convert to.
 
+Note:
+See unit_converter.py, unit_converter_v2.py, and unit_converter_v3.py for previous objectives.
 '''
 
 # Ask the user for the number of feet
