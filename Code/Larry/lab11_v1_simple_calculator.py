50db9798ff8673b34e4b0195c5e51925662380ab
larry <lmoiola@gmail.com>
2019-06-17 19:15:38 -0700

    clean up comments

--- Code/Larry/lab11_v1_simple_calculator.py
+++ Code/Larry/lab11_v1_simple_calculator.py
@@ -1,10 +1,14 @@
 # filename: lab11_v1_simple_calculator.py
 '''
 Lab 11: Simple Calculator
-Let's write a simple REPL (read evaluate print loop) calculator that can handle addition, subtraction, multiplication,
-and division. Ask the user for an operator and each operand. Don't forget that input returns a string,
+
+Let's write a simple REPL (read evaluate print loop) calculator that can handle
+addition, subtraction, multiplication, and division.
+
+Ask the user for an operator and each operand. Don't forget that input returns a string,
 which you can convert to a float using float(user_input) where user_input is the string you got from input.
 '''
+
 # Import operator function (to convert string to operator)
 import operator
 
--- Code/Larry/lab11_v3_simple_calculator.py
+++ Code/Larry/lab11_v3_simple_calculator.py
@@ -1,6 +1,7 @@
 # filename: lab11_v3_simple_calculator.py
 '''
 Lab 11: Simple Calculator
+
 Let's write a simple REPL (read evaluate print loop) calculator that can handle addition, subtraction, multiplication,
 and division. Ask the user for an operator and each operand. Don't forget that input returns a string,
 which you can convert to a float using float(user_input) where user_input is the string you got from input.
@@ -8,6 +9,7 @@ which you can convert to a float using float(user_input) where user_input is the
 Version 3 (optional)
 Allow the user to enter a full arithmetic expression and use eval to evaluate it.
 '''
+
 # Import operator function (to convert string to operator)
 import operator
 
