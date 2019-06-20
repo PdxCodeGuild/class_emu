6438e66003c78de77444597217e3f9e09e00bbc3
Kevin <wrenJTD@gmail.com>
2019-06-20 11:35:20 -0700

    Lab 18 version 2

---
+++ Code/Kevin/Lab18version2.py
@@ -0,0 +1,34 @@
+def valleys(data):
+    v_list = []
+    for i in range(1, len(data)-1):
+        current_number = data[i]
+        if current_number < data[i-1] and current_number < data[i+1]:
+            v_list.append(i)
+    return v_list
+
+def peaks(num_of_peaks):
+    p_list = []
+    for i in range(1, len(data)-1):
+        current_number = data[i]
+        if current_number > data[i-1] and current_number > data[i+1]:
+            p_list.append(i)
+    return p_list
+
+
+data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+print(f"Valleys: {valleys(data)}")
+print(f"Peaks: {peaks(data)}")
+
+
+
+#for height in [1 , 2 , 3, 4, 5, 6, 7, 8, 9]:
+
+for height in [9, 8, 7, 6, 5, 4, 3, 2, 1]:
+     row = []
+     for i in range(len(data)):
+         if data[i] >= height:
+             row.append('x')
+         else:
+             row.append(' ')
+     print(row)
+
