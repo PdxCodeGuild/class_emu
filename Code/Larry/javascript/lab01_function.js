97ca6581be31ef48974e0e2628fb4be3f48aa540
larry <lmoiola@gmail.com>
2019-07-11 12:25:45 -0700

    versions 1,2,3 of lab01-gradingV1: convert py lab to js

---
+++ Code/Larry/javascript/lab01_function.js
@@ -0,0 +1,19 @@
+function runGradingLab() {
+  let user_number = document.querySelector('#user_number').value
+  if (user_number > 100) {
+      alert("That number is too large!")
+  } else if (user_number <  0) {
+      alert("That number is less than 0!")
+  } else if (user_number >= 90 && user_number <= 100) {
+      alert("You got an A! Great job!")
+  } else if (user_number >= 80 && user_number < 90) {
+      alert("You got a B! Good job!")
+  } else if (user_number >= 70 && user_number < 80) {
+      alert("You got a C. Not bad.")
+  } else if (user_number >= 60 && user_number < 70) {
+      alert("You got a D. Umm, did you study?")
+  } else {
+      alert("That's an F. D'oh!")
+  }
+  document.querySelector('#user_number').value = ''
+}
