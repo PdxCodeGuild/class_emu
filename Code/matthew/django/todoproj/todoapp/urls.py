0e5b679453877ff487a58f91413cd267711db5d9
flux2341 <flux2341@gmail.com>
2019-07-26 11:55:07 -0700

    added new todo example

--- Code/matthew/django/todoproj/todoapp/urls.py
+++ Code/matthew/django/todoproj/todoapp/urls.py
@@ -1,8 +1,12 @@
 
+from django.contrib import admin
 from django.urls import path
+
 from . import views
 
+app_name = 'todoapp'
 urlpatterns = [
-    path('index/', views.index),
-    path('addtodo/', views.addtodo)
+    path('index/', views.index, name='index'),
+    path('savetodo/', views.savetodo, name='savetodo'),
+    path('completetodo/', views.completetodo, name='completetodo')
 ]
--- Code/matthew/django/todoproj/todoapp/views.py
+++ Code/matthew/django/todoproj/todoapp/views.py
@@ -1,21 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import timezone
 
-from .models import TodoItem
-
-
-def addtodo(request):
-    # print(request.POST)
-    todo_text = request.POST['todo_text']
-    todo_item = TodoItem(text=todo_text, completed=False)
-    todo_item.save()
-    return HttpResponseRedirect('/todoapp/index/')
+from .models import TodoItem, TodoItemType, TodoItemUrgency
 
 def index(request):
-    # for todo in TodoItem.objects.all():
-    #     print(todo.text + ' ' + str(todo.completed))
     context = {
-        'title': 'Todo List',
-        'todos': TodoItem.objects.order_by('text')
+        'items': TodoItem.objects.filter(date_completed__isnull=True),
+        'completed_items': TodoItem.objects.filter(date_completed__isnull=False),
+        'types': TodoItemType.objects.order_by('name'),
+        'urgencies': TodoItemUrgency.objects.all()
     }
     return render(request, 'todoapp/index.html', context)
+
+
+def savetodo(request):
+    todo_text = request.POST['todo_text']
+    todo_type_id = request.POST['todo_type_id']
+    todo_urgency_id = request.POST['todo_urgency_id']
+    
+    
+    todo_type = None
+    if todo_type_id != '':
+        todo_type = TodoItemType.objects.get(id=todo_type_id)
+    
+    todo_urgency = TodoItemUrgency.objects.get(id=todo_urgency_id)
+    
+    todo_item = TodoItem(text=todo_text, type=todo_type, urgency=todo_urgency, date_completed=None)
+    todo_item.save()
+    
+    return HttpResponseRedirect(reverse('todoapp:index'))
+
+
+def completetodo(request):
+    todo_item_id = request.POST['todo_item_id']
+    todo_item = TodoItem.objects.get(id=todo_item_id)
+    todo_item.date_completed = timezone.now()
+    todo_item.save()
+    return HttpResponseRedirect(reverse('todoapp:index'))
