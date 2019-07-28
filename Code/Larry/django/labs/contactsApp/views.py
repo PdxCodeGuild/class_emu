3a8cc29c4b71807d2643c99feddce2d7d44c7e9d
larry <lmoiola@gmail.com>
2019-07-28 11:55:15 -0700

    savepoint for contactsApp

---
+++ Code/Larry/django/labs/contactsApp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, reverse
+
+from .models import Contact
+
+def index(request):
+    # return HttpResponse("hello world")
+    contacts_list = Contact.objects.order_by('last_name')
+    context = {'contacts_list': contacts_list}
+    return render(request, 'contactsApp/index.html', context)
