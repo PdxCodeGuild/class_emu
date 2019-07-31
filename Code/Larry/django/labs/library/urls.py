102abef55a9d766d5d7c7babe13e3e82acc4f1a6
larry <lmoiola@gmail.com>
2019-07-31 16:08:46 -0700

    version 1 of lab04: library app

--- Code/Larry/django/labs/library/urls.py
+++ Code/Larry/django/labs/library/urls.py
@@ -6,4 +6,6 @@ app_name = 'library'
 urlpatterns = [
     # /library/
     path('', views.index, name='index'),
+    # /library/checkout/
+    path('checkout/', views.checkout, name='checkout')
 ]
--- Code/Larry/django/labs/library/views.py
+++ Code/Larry/django/labs/library/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Book, Author
 
 def index(request):
-    # return HttpResponse("Library: Hello world!")
-    books = Book.objects.order_by('-id')
-    context = {'books': books}
+    books = Book.objects.order_by('author', 'title')
+    books_sortby_title = Book.objects.filter(checked_out=False).order_by('title')
+    context = {'books': books, 'books_sortby_title': books_sortby_title}
     return render(request, 'library/index.html', context)
+
+def checkout(request):
+    book_title = request.POST['checkout_select']
+    book = Book.objects.get(title=book_title)
+    book.checked_out = True
+    book.save()
+    # return HttpResponse('ok')
+    return HttpResponseRedirect(reverse('library:index'))
