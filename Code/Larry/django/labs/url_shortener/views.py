3778f51401ea61750834e53ebe2d1373923aaab1
larry <lmoiola@gmail.com>
2019-07-31 09:51:38 -0700

    removing unused code for unique code

--- Code/Larry/django/labs/url_shortener/views.py
+++ Code/Larry/django/labs/url_shortener/views.py
@@ -15,29 +15,19 @@ def saveurl(request):
     letters = string.ascii_letters      # UPPER and lower case letters, a-Z
     digits = str(string.digits)         # integers, 0-9
     letters_digits = letters + digits   # combine letters & digits into one string
-    counter = 0
-    shorturl_code = ''
+    long_url = request.POST['long_url']
 
     while True:
-        while counter < 5:
+        shorturl_code = ''
+        for i in range(5):
             shorturl_code += random.choice(letters_digits)
-            counter += 1
-        long_url = request.POST['long_url']
-        # shorturl_code = 'qrs456' # simulate the same shorturl_code being generated
         exist_count = shortened_url.objects.filter(code=shorturl_code).count()
         if exist_count >= 1:
             print("shorturl_code already exists")
-            print(exist_count)
-            continue
         else:
             short_url = shortened_url(code=shorturl_code, long_url=long_url)
             short_url.save()
             break
-        # obj, created = shortened_url.objects.get_or_create(
-        #     code=shorturl_code,
-        #     long_url=long_url,
-        #     defaults=None,
-        # )
     return HttpResponseRedirect(reverse('url_shortener:index'))
 
 def redir_to_long_url(request, code):
